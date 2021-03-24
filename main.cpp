e3e6f371d767577c06de74e429f368c1980aa87b
Jacob Jakiemiec <jjakiemiec@gmail.com>
2021-03-23 21:57:34 -0800

    Test all of the basic shapes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 
-/*
+
 #include "BasicShapes.hpp"
 #include "CompoundShapes.hpp"
 #include "PostScriptHelper.hpp"
@@ -8,18 +8,26 @@
 
 int main()
 {
-    Circle circle1(4);
-    Rectangle rect1(3, 4);
-    LayeredShape cir1rect1(circle1, rect1);
+    Circle circle1(90);
+    Rectangle rect1(30, 40);
+	Polygon poly1(5, 40);
+	Square square1(120);
+	Triangle triangle1(180);
+    //LayeredShape cir1rect1(circle1, rect1);
 
 	ofstream fs;
 	fs.open("example.ps");
-	int x = 70;
-	int y = 50;
-	fs << header() << moveto(x, y) << rlineto(90, 90) << rotate(45) << rlineto(90, 0) << stroke();
-	fs << moveto(500, 500) << lineto(550, 500) << stroke() << rmoveto(0, 50) << lineto(500, 550) << stroke() << showpage();
+	int x = 300;
+	int y = 300;
+	fs << header() << moveto(x, y);
+	rect1.doPostScript(fs);
+	poly1.doPostScript(fs);
+	circle1.doPostScript(fs);
+	square1.doPostScript(fs);
+	triangle1.doPostScript(fs);
+	fs << showpage();
 	fs.close();
     return 0;
 }
-*/
+
 
