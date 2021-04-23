33de7a99b007111b01cb61560cd91bfab5586c22
timothyakline <44985180+timothyakline@users.noreply.github.com>
2021-04-23 15:20:01 -0800

    Made helpers for PostScript bridge

--- include/PostScript_bridge.hpp
+++ include/PostScript_bridge.hpp
@@ -3,11 +3,12 @@
 // AUTH: Jacob Jakiemiec
 //		 Riley Maranville
 //       Cody Abad
+//		 Timothy Albert Kline
 // CRSE: F372 - Software Construction
 // PROF: Dr. Chris Hartman
 // STRT: 07 March 2021
-// UPDT: 22 March 2021
-// VERS: 1.0
+// UPDT: 23 March 2021
+// VERS: 2.0
 
 #ifndef POST_SCRIPT_BRIDGE_HPP
 #define POST_SCRIPT_BRIDGE_HPP
@@ -22,24 +23,29 @@ using std::to_string;
 #define CenterX 306
 #define CenterY 396
 
-auto moveto(int x, int y) -> string;
-auto rmoveto(int x, int y) -> string;
-auto lineto(int x, int y) -> string;
-auto rlineto(int x, int y) -> string;
-auto arc(int r, int s, int f) -> string;
-auto rotate(int angle) -> string;
-auto translate(int x, int y) -> string;
-auto scale(double fx, double fy) -> string;
-auto newpath() -> string;
-auto stroke() -> string;
-auto showpage() -> string;
 auto header() -> string;
+auto newpath() -> string;
+auto currentpoint() -> string;
+auto stroke() -> string;
+auto closepath() -> string;
+auto showpage() -> string;
+
+auto moveto(const int &x, const int &y) -> string;
+auto rmoveto(const int &x, const int &y) -> string;
+auto lineto(const int &x, const int &y) -> string;
+auto rlineto(const int &x, const int &y) -> string;
 auto gsave() -> string;
 auto grestore() -> string;
+
+auto point(const int &x, const int &y) -> string;
+auto arc(const double &r, const int &s, const int &f) -> string;
 auto center() -> string;
-auto currentpoint() -> string;
-auto closepath() -> string;
-auto setrgbcolor(double r, double g, double b) -> string;
+auto translate() -> string;
+auto translate(const int &x, const int &y) -> string;
+auto rotate(const int &angle) -> string;
+auto scale(const double &fx, const double &fy) -> string;
+
+auto setrgbcolor(const double &r, const double &g, const double &b) -> string;
 auto fill() -> string;
 
 #endif
